e11a5c55bc9a40a1ce4dce24c401fa8725ef2422
keithwang <keithwang@objectivasoftware.com>
2021-02-03 18:54:05 +0800

    DEMO-001: added electronics extensions

---
+++ hybris6701b2ctelco/hybris/bin/custom/electronicsonline/electronicsonlinecockpits/src/de/hybris/electronics/cockpits/constants/ElectronicsonlineCockpitsConstants.java
@@ -0,0 +1,26 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2018 SAP SE or an SAP affiliate company.  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package de.hybris.electronics.cockpits.constants;
+
+/**
+ * Global class for all ElectronicsonlineCockpits constants. You can add global constants for your extension into this class.
+ */
+public final class ElectronicsonlineCockpitsConstants extends GeneratedElectronicsonlineCockpitsConstants
+{
+	public static final String EXTENSIONNAME = "electronicsonlinecockpits";
+
+	public static final String JASPER_REPORTS_MEDIA_FOLDER = "jasperreports";
+
+	private ElectronicsonlineCockpitsConstants()
+	{
+		// empty
+	}
+}
---
+++ hybris6701b2ctelco/hybris/bin/custom/electronicsonline/electronicsonlineinitialdata/src/de/hybris/electronics/initialdata/constants/ElectronicsonlineInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2018 SAP SE or an SAP affiliate company.  All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package de.hybris.electronics.initialdata.constants;
+
+/**
+ * Global class for all ElectronicsonlineInitialData constants.
+ */
+public final class ElectronicsonlineInitialDataConstants extends GeneratedElectronicsonlineInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "electronicsonlineinitialdata";
+
+	private ElectronicsonlineInitialDataConstants()
+	{
+		//empty
+	}
+}
